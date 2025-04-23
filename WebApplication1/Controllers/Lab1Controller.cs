bb3d280f3e542120d6f6d4a3dd45c507203a1ffa
User <User@DESKTOP-1Q9K946>
2025-04-23 15:54:58 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/Lab1Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class Lab1Controller : Controller
+    {
+        public List<string> GetVegetablesList()
+        {
+            List<string> vegetables = new List<string>();
+            vegetables.Add("Томат");
+            vegetables.Add("Картофель");
+            vegetables.Add("Кабачок");
+            vegetables.Add("Баклажан");
+            vegetables.Add("Капуста");
+            vegetables.Add("Броколи");
+            
+            return vegetables;
+        }
+        // GET: Lab1
+        public ActionResult FirstViewMethod()
+        {
+            List<string> vegetables = GetVegetablesList();
+            return View(vegetables);
+        }
+    }
+}
